7e5c843d54db43050643d21bed9b00b3176e3ea2
miptivan <miptivan@gmail.com>
2022-09-15 18:34:23 +0300

    add modification for double

--- src/Sum.java
+++ src/Sum.java
@@ -1,29 +1,26 @@
 import java.util.*;
 
-public class Sum {
+public class SumDouble {
     public static void main(String[] args) {
-        int sum = 0;
+        double sum = 0;
         for (int i = 0; i < args.length; i++) {
             int lastSpace = 0;
             int end = args[i].length();
             int j = 0;
-            int point = 0;
             while (j < end) {
                 if (Character.isWhitespace(args[i].charAt(j))) {
                     if (lastSpace < j) {
-                        sum += Integer.parseInt(args[i].substring(lastSpace, j));
+                        sum += Double.parseDouble(args[i].substring(lastSpace, j));
                     }
                     lastSpace = j + 1;
                 }
                 j++;
-                point = j;
             }
 
             if (lastSpace < j) {
-                sum += Integer.parseInt(args[i].substring(lastSpace, j));
+                sum += Double.parseDouble(args[i].substring(lastSpace, j));
             }
         }
         System.out.println(sum);
     }
 }
-
