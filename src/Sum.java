1da647d85d828a2a46e897d0fe04e8b13a623f0b
miptivan <miptivan@gmail.com>
2022-09-15 17:48:54 +0300

    without slowly concat

--- src/Sum.java
+++ src/Sum.java
@@ -3,25 +3,27 @@ import java.util.*;
 public class Sum {
     public static void main(String[] args) {
         int sum = 0;
-        String strNumber = "";
         for (int i = 0; i < args.length; i++) {
-            for (int j = 0; j < args[i].length(); j++) {
-                char c = args[i].charAt(j);
-                if ((c >= '0' && c <= '9') || c == '+' || c == '-') {
-                    strNumber += c;
-                } else {
-                    if (strNumber != "") {  // если встретился пробел
-                        System.err.println(strNumber);
-                        sum += Integer.parseInt(strNumber);  // суммируем что насчитали до пробела
-                        strNumber = "";
+            int lastSpace = 0;
+            int end = args[i].length();
+            int j = 0;
+            int point = 0;
+            while (j < end) {
+                if (Character.isWhitespace(args[i].charAt(j))) {
+                    if (lastSpace < j) {
+                        sum += Integer.parseInt(args[i].substring(lastSpace, j));
                     }
+                    lastSpace = j + 1;
                 }
+                j++;
+                point = j;
             }
-            if ( strNumber != "") {
-                sum += Integer.parseInt(strNumber);
+
+            if (lastSpace < j) {
+                sum += Integer.parseInt(args[i].substring(lastSpace, j));
             }
-            strNumber = "";
         }
         System.out.println(sum);
     }
 }
+
